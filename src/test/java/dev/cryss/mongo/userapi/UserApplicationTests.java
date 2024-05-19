023e7dc0976fde606561781b780b3f94c4a5ebdb
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-05-19 17:24:03 -0300

    projeto inicial

---
+++ src/test/java/dev/cryss/mongo/userapi/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cryss.mongo.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
